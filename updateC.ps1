330bc060accd7e3281d7255bd282cda4f4587a56
physics11688 <t.yamamoto116@gmail.com>
2025-10-26 08:56:49 +0900

    updateCのインストールディレクトリ変更

--- updateC.ps1
+++ updateC.ps1
@@ -47,6 +47,8 @@ catch {
     Write-Warning "LLVM のアップデートでエラーが発生しました: $_"
 }
 
+Add-ToUserPath -NewPath "C:\Program Files\LLVM\bin"
+
 Write-Host "`n=== MinGW の最新版URL取得 ==="
 try {
     $URL = (curl.exe -s $githubApiUrl |
