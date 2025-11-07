1c27e9f7351c016f876ed8b6bc212675d6fee2d2
physics11688 <t.yamamoto116@gmail.com>
2025-11-07 14:20:04 +0900

    zshseutupファイル追加

--- remodeling_zsh.sh
+++ remodeling_zsh.sh
@@ -5,9 +5,33 @@ export LS_COLORS="bd=38;5;68:ca=38;5;17:cd=38;5;113;1:di=38;5;30:do=38;5;127:ex=
 
 export LANG=ja_JP.UTF-8
 
-# Set up the prompt
-autoload -Uz promptinit
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
+fi
+
+
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+autoload -Uz _zinit
 promptinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-bin-gem-node \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-rust
+
+
+### End of Zinit's installer chunk
+
+
 
 # Use modern completion system
 autoload -Uz compinit
