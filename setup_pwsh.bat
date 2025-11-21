93b182d5c97c64df20ee8511c41bf4d463d47282
physics11688 <t.yamamoto116@gmail.com>
2025-11-21 21:51:25 +0900

    bat修正

--- setup_pwsh.bat
+++ setup_pwsh.bat
@@ -1,14 +1,13 @@
 @echo off
 chcp 65001 >nul
-setx PATH "%PATH%;C:\Users\%USERNAME%\AppData\Local\Microsoft\WindowsApps"
+
 echo PowerShell 7 (pwsh) の存在を確認しています...
 
-set "PATH=%PATH%;C:\Users\%USERNAME%\AppData\Local\Microsoft\WindowsApps"
 where.exe pwsh >nul 2>nul
 if %errorlevel% neq 0 (
     echo PowerShell 7 が見つかりません。winget でインストールを試みます...
 
-    where winget >nul 2>nul
+    "%LOCALAPPDATA%\Microsoft\WindowsApps\winget.exe" --version >nul 2>nul
     if errorlevel 1 (
         echo winget が見つかりません。手動で PowerShell 7 をインストールしてください。
 
@@ -16,21 +15,21 @@ if %errorlevel% neq 0 (
         exit /b
         ) else (
         echo wingetが見つかりました。
-        winget install uutils.coreutils
+
     )
 
 
     echo PowerShell 7 をインストール中です...
-    winget install --id Microsoft.PowerShell --source winget --accept-package-agreements --accept-source-agreements
+    "C:\Users\%USERNAME%\AppData\Local\Microsoft\WindowsApps\winget.exe" install --id Microsoft.PowerShell --source winget --accept-package-agreements --accept-source-agreements
+    "C:\Users\%USERNAME%\AppData\Local\Microsoft\WindowsApps\winget.exe" install uutils.coreutils
     start "" wt powershell -Command "exit"
 
 )
 
-set "PATH=%PATH%;C:\Program Files\PowerShell\7"
 echo PowerShell 7 が見つかりました。実行ポリシーを RemoteSigned に設定します...
-pwsh -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
+"C:\Program Files\PowerShell\7\pwsh.exe" -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
 echo 設定が完了しました。現在のポリシーを確認します...
-pwsh -Command "Get-ExecutionPolicy -Scope CurrentUser"
+"C:\Program Files\PowerShell\7\pwsh.exe" -Command "Get-ExecutionPolicy -Scope CurrentUser"
 echo ============================================
 echo 上記が RemoteSigned なら成功です。
 echo ============================================
@@ -49,7 +48,7 @@ set "TEMP_PY=%TEMP%\temp_script.py"
 curl -s %PY_URL% -o "%TEMP_PY%"
 
 :: Python スクリプトを実行
-python "%TEMP_PY%"
+py "%TEMP_PY%"
 
 :: 一時ファイルを削除
 del "%TEMP_PY%"
@@ -58,5 +57,5 @@ endlocal
 
 
 echo PowerShell スクリプトをダウンロードして実行します...
-pwsh -Command "iex (Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/physics11688/miscellaneous/main/setup_pwsh.ps1').Content"
+"C:\Program Files\PowerShell\7\pwsh.exe" -Command "iex (Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/physics11688/miscellaneous/main/setup_pwsh.ps1').Content"
 pause
